90eead70fcb2a85e3c4a2f4cdcb289ade129aa2b
Sandra <02sm.culqui.c@gmail.com>
2024-04-22 21:07:33 -0500

    se agrego el aside y el navbar a los includes

---
+++ includes/navbar.php
@@ -0,0 +1,21 @@
+<section role="main" class="content-body">
+					<header class="page-header">
+						<h2>Blank Page</h2>
+
+						<div class="right-wrapper text-end">
+							<ol class="breadcrumbs">
+								<li>
+									<a href="index.html">
+										<i class="bx bx-home-alt"></i>
+									</a>
+								</li>
+
+								<li><span>Pages</span></li>
+
+								<li><span>Blank Page</span></li>
+
+							</ol>
+
+							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fas fa-chevron-left"></i></a>
+						</div>
+					</header>
--- index.php
+++ index.php
@@ -47,95 +47,9 @@
 
 			<div class="inner-wrapper">
 				<!-- start: sidebar -->
-				<aside id="sidebar-left" class="sidebar-left">
-
-				    <div class="sidebar-header">
-				        <div class="sidebar-title">
-				            Navigation
-				        </div>
-				        <div class="sidebar-toggle d-none d-md-block" data-toggle-class="sidebar-left-collapsed" data-target="html" data-fire-event="sidebar-left-toggle">
-				            <i class="fas fa-bars" aria-label="Toggle sidebar"></i>
-				        </div>
-				    </div>
-
-				    <div class="nano">
-				        <div class="nano-content">
-				            <nav id="menu" class="nav-main" role="navigation">
-
-				                <ul class="nav nav-main">
-				                    <li>
-				                        <a class="nav-link" href="layouts-default.html">
-				                            <i class="bx bx-home-alt" aria-hidden="true"></i>
-				                            <span>Dashboard</span>
-				                        </a>                        
-				                    </li>
-									<li>
-				                        <a class="nav-link" href="layouts-default.html">
-				                            <i class="bx bx-home-alt" aria-hidden="true"></i>
-				                            <span>Dashboard</span>
-				                        </a>                        
-				                    </li>
-									<li>
-				                        <a class="nav-link" href="layouts-default.html">
-				                            <i class="bx bx-home-alt" aria-hidden="true"></i>
-				                            <span>Dashboard</span>
-				                        </a>                        
-				                    </li>
-									<li>
-				                        <a class="nav-link" href="layouts-default.html">
-				                            <i class="bx bx-home-alt" aria-hidden="true"></i>
-				                            <span>Dashboard</span>
-				                        </a>                        
-				                    </li>      
-									<li>
-				                        <a class="nav-link" href="layouts-default.html">
-				                            <i class="bx bx-home-alt" aria-hidden="true"></i>
-				                            <span>Dashboard</span>
-				                        </a>                        
-				                    </li>      									   
-				                </ul>
-				            </nav>
-
-				        </div>
-
-				        <script>
-				            // Maintain Scroll Position
-				            if (typeof localStorage !== 'undefined') {
-				                if (localStorage.getItem('sidebar-left-position') !== null) {
-				                    var initialPosition = localStorage.getItem('sidebar-left-position'),
-				                        sidebarLeft = document.querySelector('#sidebar-left .nano-content');
-
-				                    sidebarLeft.scrollTop = initialPosition;
-				                }
-				            }
-				        </script>
-
-				    </div>
-
-				</aside>
+				<?php require './includes/aside.php' ?>
 				<!-- end: sidebar -->
-
-				<section role="main" class="content-body">
-					<header class="page-header">
-						<h2>Blank Page</h2>
-
-						<div class="right-wrapper text-end">
-							<ol class="breadcrumbs">
-								<li>
-									<a href="index.html">
-										<i class="bx bx-home-alt"></i>
-									</a>
-								</li>
-
-								<li><span>Pages</span></li>
-
-								<li><span>Blank Page</span></li>
-
-							</ol>
-
-							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fas fa-chevron-left"></i></a>
-						</div>
-					</header>
+				<?php require './includes/navbar.php' ?>
 
 					<!-- start: page -->
 					<!-- end: page -->
