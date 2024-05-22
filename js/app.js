0f58fdaf6d88dda9e0cbc46c20e2e47a12b2fe8c
Antony Alfredo Culqui Carranza <antonyalfredoculquicarranza@MacBook-Pro-de-Antony.local>
2024-05-22 10:56:11 -0500

    se cambio formato de fecha

--- js/app.js
+++ js/app.js
@@ -331,7 +331,7 @@ const listar_registros = function () {
                         }
 
                         html = html + `
-                    <tr> <td class='text-center'>${registro.id}</td><td class='d-none'>${registro.usuario_id}</td><td>${registro.promotor}</td><td>${registro.tipo}</td> <td>${registro.indicativo}</td><td >${registro.fecha.split(" ")[0]}</td>
+                    <tr> <td class='text-center'>${registro.id}</td><td class='d-none'>${registro.usuario_id}</td><td>${registro.promotor}</td><td>${registro.tipo}</td> <td>${registro.indicativo}</td><td >${registro.fecha}</td>
                     <td>${registro.clasificacion}</td><td>${registro.asunto}</td> <td>${registro.recibido}</td><td class='text-center' width='100px'><a href="./pdf/view?file=${url_document}" target='_blank'><i class='bx bxs-file-pdf bx-md'></i></a></td><td class='d-none'>${registro.decreto}</td><td class='d-none'>${registro.obs_admin}</td><td class='d-none'>${registro.obs_validador}</td>${firma_gdh}<td class='d-none'>${registro.firma_gdh_fecha}</td><td class='d-none'>${registro.firma_gdh_usuario}</td><td class='d-none'>${registro.firma_destino_fecha}</td>${firma_destino}</tr>`;
                         position++
                     })
